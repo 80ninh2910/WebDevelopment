0a8ef5e3179dacc74250dbd5798e15c507e41321
80ninh2910 <ninhqa234112e@st.uel.edu.vn>
2025-10-14 13:52:35 +0700

    ab
    ds

--- learnjs/js/mainjs.js
+++ learnjs/js/mainjs.js
@@ -1,4 +1,19 @@
 function find_fib(n){
     if (n<=2) return 1;
-    return find_fib(n-1)+find_fib(n-2);
+    return find_fib(n-1) + find_fib(n-2);
+}
+function invoke_fib(n,panel){
+    fib=find_fib(n);
+    panel.innerHTML=fib;
+}
+function invoke_list_fib(n,panel){
+    panel.innerHTML="";
+    for (i=1;i<=n;i++){
+        r=Math.floor(Math.random()*256);
+        g=Math.floor(Math.random()*256);
+        b=Math.floor(Math.random()*256);
+        bg="rgb("+r+","+g+","+b+")";
+        p="<p style='background-color:"+bg+"'>"+find_fib(i)+"</p>";
+        panel.innerHTML+=p;
+    }
 }
