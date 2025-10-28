a7bdcf9f6c3f225ed9bc0ba69bbbb2de35e5cabb
80ninh2910 <ninhqa234112e@st.uel.edu.vn>
2025-10-28 08:00:07 +0700

    xc
    xzcxv

--- Exercise9/obj/Debug/net8.0/Exercise9.AssemblyInfo.cs
+++ Exercise9/obj/Debug/net8.0/Exercise9.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6602a81537fcacebc9e2e923c548b2afd85de32e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9dbd072d02d21806162ab186ed3691786dbfea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
